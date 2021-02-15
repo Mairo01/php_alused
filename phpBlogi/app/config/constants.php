39972d09fb347b5d16dba528ed03690d9aabac97
Mairo01 <71321098+Mairo01@users.noreply.github.com>
2021-02-15 23:00:47 +0200

    Create constants.php

---
+++ phpBlogi/app/config/constants.php
@@ -0,0 +1,19 @@
+<?php
+// Project name
+define('PROJECT_NAME', 'blogi');
+// App root dir
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL root
+define('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
+
+// db params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'mairo000');
+define('DB_PASS', '123456');
+define('DB_NAME', 'blog');
+/*
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'blogi');
+*/
