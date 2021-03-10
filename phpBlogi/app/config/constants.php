6ab527bb35a6fff4307982f6e3b751e94fc5f926
Mairo01 <71321098+Mairo01@users.noreply.github.com>
2021-03-10 19:10:25 +0200

    Update constants.php

--- phpBlogi/app/config/constants.php
+++ phpBlogi/app/config/constants.php
@@ -5,12 +5,17 @@ define('PROJECT_NAME', 'blogi');
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL root
 define('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'blog_vs20_1');
 
-// db params
+
+/*// db params
 define('DB_HOST', 'localhost');
 define('DB_USER', 'mairo000');
 define('DB_PASS', '123456');
-define('DB_NAME', 'blog');
+define('DB_NAME', 'blog');*/
 /*
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
